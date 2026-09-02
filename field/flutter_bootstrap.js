86838e789354c4d2c498d907059310a46aba7702
Josh Johnson <118121166+murderszn@users.noreply.github.com>
2026-09-02 09:39:41 -0500

    Refactor landing page design: unified dark mode, interactive anatomy diagram, and button consistency

--- field/flutter_bootstrap.js
+++ field/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "218090663" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3738456367" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
